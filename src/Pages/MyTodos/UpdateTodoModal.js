b543df2c3455c5f9332e7f579d21f61ffa58f62a
sdshuvo98 <hellosdshuvo@gamil.com>
2022-08-03 00:30:15 +0600

    handle update todo

--- src/Pages/MyTodos/UpdateTodoModal.js
+++ src/Pages/MyTodos/UpdateTodoModal.js
@@ -1,16 +1,47 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const UpdateTodoModal = () => {
+const UpdateTodoModal = ({ updatingTodo, setUpdatingTodo }) => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const date = e.target.date.value;
+        const heading = e.target.heading.value;
+        const description = e.target.description.value;
+        const todo = {
+            date,
+            heading,
+            description,
+            isCompleted: false
+        }
+        fetch(`http://localhost:5000/todo/update/${updatingTodo._id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({ todo })
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    toast.success("Todo is updated successfully!")
+                }
+            })
+        setUpdatingTodo(null)
+    }
+
     return (
         <section>
             <input type="checkbox" id="update-todo-modal" className="modal-toggle" />
             <div className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box">
-                    <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-                    <div className="modal-action">
-                        <label for="update-todo-modal" className="btn">Yay!</label>
-                    </div>
+                    <h3 className="font-bold text-lg mb-3">You are updating {updatingTodo.heading}!</h3>
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" name='date' defaultValue={updatingTodo.date} className="input input-bordered w-full mb-3" required />
+                        <input type="text" name='heading' defaultValue={updatingTodo.heading} placeholder="Todo Heading" className="input input-bordered w-full mb-3" required />
+                        <textarea name='description' defaultValue={updatingTodo.description} placeholder="Description" className="input input-bordered w-full mb-3" required />
+                        <input type="submit" value="Add Todo" className="btn btn-primary w-full mb-3" />
+                    </form>
                 </div>
             </div>
         </section>
