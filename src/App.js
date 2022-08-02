79c219908e2bfd87f37474278569e93728dc2d68
sdshuvo98 <hellosdshuvo@gamil.com>
2022-08-02 20:13:54 +0600

    developed navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import MyTodos from './Pages/MyTodos/MyTodos';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/todos' element={<MyTodos />} />
