901d7a1178cc3f7035d2c5efde9e29085ebaec4a
sdshuvo98 <hellosdshuvo@gamil.com>
2022-08-02 22:57:43 +0600

    developed my todos route

--- src/Pages/MyTodos/MyTodos.js
+++ src/Pages/MyTodos/MyTodos.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-function MyTodos() {
+const MyTodos = () => {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/todo')
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                setTodos(data);
+            })
+    }, [todos])
     return (
-        <div>MyTodos</div>
-    )
-}
+        <section className='bg-slate-100 p-4 lg:p-8 h-screen'>
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        {/* <!-- head --> */}
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Todo</th>
+                                <th>Date</th>
+                                <th>Description</th>
+                                <th className='text-center'>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                todos.map((todo, index) => <tr key={todo._id} className='hover'>
+                                    <th>{index + 1}</th>
+                                    <td>{todo.heading}</td>
+                                    <td>{todo.date}</td>
+                                    <td>{todo.description}</td>
+                                    <td className='flex justify-center gap-4'>
+                                        <button className='btn btn-primary btn-outline'>Complete</button>
+                                        <button className='btn btn-error btn-outline'>Delete</button>
+                                    </td>
+                                </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section >
+    );
+};
 
-export default MyTodos
+export default MyTodos;
